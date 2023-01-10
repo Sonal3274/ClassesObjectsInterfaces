0d43f8b4fae77a9793319a45b45c9a4bb5756d70
Sonal Dhage <sonal.dhage@thermondo.de>
2023-01-10 09:17:37 +0100

    created simple interface example

---
+++ src/main/kotlin/Interface.kt
@@ -0,0 +1,27 @@
+interface MyInterface {
+
+    val test: Int
+
+    fun foo() : String
+
+    fun hello() {
+        println("Hello there, pal!")
+    }
+}
+
+class InterfaceImp : MyInterface {
+    override val test: Int = 75
+    override fun foo() = "This is the foo function"
+}
+
+fun main() {
+    val obj = InterfaceImp()
+
+    println("test = ${obj.test}")
+    print("Calling hello(): ")
+
+    obj.hello()
+
+    print("Calling and printing foo(): ")
+    println(obj.foo())
+}
