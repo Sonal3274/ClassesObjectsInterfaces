4b0c28cf1a846d78411fa607516c014dfbc63015
Sonal Dhage <sonal.dhage@thermondo.de>
2023-01-13 17:28:13 +0100

    Created example for constructors

---
+++ src/main/kotlin/Constructors.kt
@@ -0,0 +1,29 @@
+import java.sql.Driver
+
+/*
+* Constructors are used to define the default value for the objects.
+* This example contains primary constructors, secondary constructors and init block.
+*/
+
+fun main() {
+    var car = Automobile("Car", 4, 5, true)
+    println(car.airbags)
+    println(car.tyres)
+    println(car.maxSeating)
+
+
+    var Person = Person("A", 20)
+    println(Person.name)
+    println(Person.age)
+}
+
+class Automobile(val name: String, val tyres: Int, val maxSeating: Int, hasAirbags: Boolean) {
+    var airbags = hasAirbags
+}
+
+class Empty
+
+class Person(nameParam: String, ageParam: Int) {
+    val name: String = "$nameParam - Clan"
+    val age: Int = ageParam
+}
